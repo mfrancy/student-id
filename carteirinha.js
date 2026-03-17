318e91ec2a95a8ccc09709dc29c40cedfb72f957
Mariana Francisco <marianalopes697@gmail.com>
2026-03-17 14:13:58 -0300

    feat: adiciona integração com db de estudantes

---
+++ carteirinha.js
@@ -0,0 +1,19 @@
+const aluno = JSON.parse(localStorage.getItem("aluno"))
+
+if(!aluno){
+
+window.location.href = "index.html"
+
+}else{
+
+document.getElementById("nomeAluno").innerText = aluno.nome
+document.getElementById("raAluno").innerText = aluno.ra
+document.getElementById("cursoAluno").innerText = aluno.curso
+document.getElementById("statusAluno").innerText = aluno.status
+document.getElementById("matriculaAluno").innerText = aluno.matricula
+
+if(aluno.foto){
+document.getElementById("fotoAluno").src = aluno.foto
+}
+
+}
---
+++ login.js
@@ -0,0 +1,37 @@
+async function login() {
+
+    const ra = document.getElementById("ra").value
+    const senha = document.getElementById("senha").value
+
+    try {
+
+        const response = await fetch("https://mfrancy.github.io/json-db/db/students.json")
+        const alunos = await response.json()
+
+        const aluno = alunos[ra]
+
+        if (aluno && aluno.senha === senha) {
+
+            const alunoCompleto = {
+                ra: ra,
+                ...aluno
+            }
+
+            localStorage.setItem("aluno", JSON.stringify(alunoCompleto))
+
+            window.location.href = "carteirinha.html"
+
+        } else {
+
+            document.getElementById("erro").innerText = "RA ou senha inválidos"
+
+        }
+
+    } catch (e) {
+
+        console.error(e)
+        document.getElementById("erro").innerText = "Erro ao conectar com o servidor"
+
+    }
+
+}
